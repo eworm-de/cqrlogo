81e22becffcccf98e89a2c7ec5d33c4d12f8e9ed
Christian Hesse <mail@eworm.de>
2013-06-04 18:47:44 +0200

    add comments abount defines

--- cqrlogo.c
+++ cqrlogo.c
@@ -13,7 +13,14 @@
 #include <gdk-pixbuf/gdk-pixbuf.h>
 #include <qrencode.h>
 
+/* pixels are scaled up by this factor */
 #define QRCODE_SCALE	2
+/* error correction level used for QR code
+ * possible values: QR_ECLEVEL_L (lowest, about 7% error can be corrected)
+ *                  QR_ECLEVEL_M (about 15%)
+ *                  QR_ECLEVEL_Q (about 25%)
+ *                  QR_ECLEVEL_H (highest, about 30%)
+ * image size raises with higher levels */
 #define QRCODE_LEVEL	QR_ECLEVEL_L
 
 GdkPixbuf * encode_qrcode (char *text) {
